3f54c18a9796be88a8949cda48458abcb084425c
Dávid Szabó <david@collock.com>
2021-02-10 18:29:31 +0100

    add dog picure

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    <>
+    <div>
       <div>Ez bizony egy kurva jó oldal lett Dávid</div>
       <img
         src="https://dog.ceo/api/breeds/image/random Fetch!
 "
       ></img>
-    </>
+    </div>
   );
 });
 
