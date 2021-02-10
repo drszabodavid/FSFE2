ec490919bbc6308cfbdcf00790406e293118add0
Dávid Szabó <david@collock.com>
2021-02-10 18:22:02 +0100

    add dog picure

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res) => {
-
-	res.send("Nice work David");
-
+app.get("/", (req, res) => {
+  res.send(
+    <>
+      <div>Ez bizony egy kurva jó oldal lett Dávid</div>
+      <img
+        src="https://dog.ceo/api/breeds/image/random Fetch!
+"
+      ></img>
+    </>
+  );
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`)); 
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
