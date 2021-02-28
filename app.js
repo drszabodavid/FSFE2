922d72c024ee8d1a7fc82d4ea22d4b6e191221f0
Dávid Szabó <david@collock.com>
2021-02-28 16:54:09 +0100

    fixed

--- app.js
+++ app.js
@@ -6,10 +6,4 @@ app.get("/", (req, res) => {
   res.send("Nice work David!");
 });
 
-app.get("/demo", (req, res) => {
-  res.set("X-full-stack", "4life");
-  res.status(418);
-  res.send("I prefer coffee");
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
