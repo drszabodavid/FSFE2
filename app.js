6c08ef61575c989767a53741f1633fd7b6157154
Dávid Szabó <david@collock.com>
2021-02-28 15:42:25 +0100

    teapot

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Nice work David, great server");
+  res.send("Nice work David");
+});
+
+app.get("/teapot", (req, res) => {
+  res.send("Nice work teapot");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
