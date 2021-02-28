e6ccca95b161bd81d659f2e5cbf5a152dfe8e7b8
Dávid Szabó <david@collock.com>
2021-02-28 16:02:16 +0100

    test

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Nice work David");
+  res.send("Nice work David!");
+});
+
+app.get("/teapot", (req, res) => {
+  res.set("X-full-stack", "4life");
+  res.statusMessage(418);
+  res.send("I prefer coffee");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
